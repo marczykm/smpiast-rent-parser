cd605f066a429188f738715cc3a4948535dc4ca3
Marcin Marczyk <m.l.marczyk@gmail.com>
2018-03-25 20:38:11 +0200

    added pushbullet notifications

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import getopt
 from lxml import html
+from pushbullet import Pushbullet
 
 def usage():
 	print 'app.py -u <username> -p <password>'
@@ -61,6 +62,25 @@ def main(argv):
 		payment=last_summary_raw[3],
 		balance=last_summary_raw[4]
 	)
+
+	try:
+		f = open('last_title', 'r+')
+	except IOError:
+		print 'no file'
+		f = open('last_title', 'w+')
+
+	got_new = f.readline().strip()!=last_summary['title'].strip() 
+	if got_new:
+		# send notification
+		pb_file = open('pb_api_key', 'r')
+		pb_api_key = pb_file.read()
+		pb_file.close()
+		pb = Pushbullet(pb_api_key)
+		pb.push_note("SM Piast", 'Tytul: ' + last_summary['title'] + '\n' + 'Saldo: ' + last_summary['balance'])
+		f.close()
+		f = open('last_title', 'w+')
+		f.write(last_summary['title'])
+	f.close()
 
 	print('Downloaded last summary:')
 	print(last_summary)
